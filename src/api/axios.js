3504bc74aa170adee96d29ef12c4eda948e09b86
unknown <najibnasution6.com>
2026-09-19 17:08:14 +0700

    add vercel config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Interceptor: Menempelkan Token JWT otomatis dari LocalStorage jika ada
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
